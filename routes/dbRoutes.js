4bbd7057c7ce5bab1330902c94cf9b222e48a781
McDonald <wmcdonald@una.edu>
2026-03-12 13:34:18 -0500

    Restarted, got it working

--- routes/dbRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getMessage } from '../controllers/dbControllers.js';
-
-const router = express.Router();
-
-router.get('/api/message', getMessage);
-
-export default router;
